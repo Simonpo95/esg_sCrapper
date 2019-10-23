9bb57c20b1d1461428a046d3dafa5c9f3c7fc726
Simon <halimisimon@gmail.com>
2019-10-23 21:57:21 +0200

    WORK

--- supervisor.h
+++ supervisor.h
@@ -6,7 +6,7 @@
 #include <string.h>
 #include <curl/curl.h>
 #include <stdlib.h>
-
+#include <math.h>
 #include "curler.h"
 
 #endif
---
+++ tmain.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+typedef struct Cola {
+    int taux_gaz;
+    int taux_sucre;
+    char * marque;
+    char * descriptif;
+
+
+
+} cola;
+
+
+int main(int argc, char ** argv){
+
+}
