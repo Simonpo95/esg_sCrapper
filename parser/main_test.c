8b36863f3a3a795b4ad822bec942367533657b67
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2019-11-09 14:23:14 +0100

    ajout du write_till_end quasi fonctionnelle

--- parser/main_test.c
+++ parser/main_test.c
@@ -5,35 +5,62 @@
 #include <math.h>
 
 #include "struct_Tag.c"
-#include "struct_Tab.c"
+#include "struct_IntTab.c"
+#include "struct_StrTab.c"
 #include "file_parser.c"
-
-//int seek_start_Tag();
 
 int main(int argc, char ** argv){
 
+//    char **str_tab = malloc(sizeof(char *) * 100);
+//
+//    for (int o = 0; o > 100; o++) {
+//        str_tab[o] = malloc(sizeof(char *) * 100);
+//    }
+//
+//    StrTab* strTab = create_StrTab(str_tab);
+//    for(int i = 0; i < 42; i++)
+//    {
+//        addToStrTab(strTab, "test");
+//    }
+
+//    printStrTab(*strTab);
+
     Tag tabTag[2];
-    IntTab tabIntTab[2];
+//    IntTab tabIntTab[2];
 
     Tag* link = create_Tag("<a", "</a>");
     Tag* pict = create_Tag("<img", ">");
 
     tabTag[0] = *link;
     tabTag[1] = *pict;
-
-//    printTabTag(tabTag);
-
+//
+////    printTabTag(tabTag);
+//
     IntTab* pr = seek_start_Tag(tabTag, "file_content.txt");
 //    IntTab* pr = seek_start_Tag(tabTag, "test_file.txt");
-    int i = 0;
+//    int i = 0;
+//
+////    IntTab* tab1 = create_IntTab(pr);
+////    IntTab* tab2 = create_IntTab(pr);
+//
+//    tabIntTab[0] = *pr;
+//    tabIntTab[1] = *pr;
+//    char** hello = malloc(sizeof(char*)*1000);
+////    prIntTabIntTab(tabIntTab);
+//
+    StrTab* d = write_till_end(pr, tabTag,"file_content.txt");
+//    printf("\n\n\n\n");
+//    printStrTab(*d);
 
-//    IntTab* tab1 = create_IntTab(pr);
-//    IntTab* tab2 = create_IntTab(pr);
 
-    tabIntTab[0] = *pr;
-    tabIntTab[1] = *pr;
+//    int y = 0;
+//    while(y<10)
+//    {
+//        char* ph = d.content_tab[y];
+//        printf("%s\n", ph);
+//        y++;
+//    }
 
-    prIntTabIntTab(tabIntTab);
 
 //    while(i < 11)
 //    {
@@ -42,5 +69,4 @@ int main(int argc, char ** argv){
 //        i++;
 //    }
 
-    return 0;
 }
