84cc0d186c31ea2e511cfab34657f32b09b442eb
Simon <halimisimon@gmail.com>
2019-11-17 18:21:55 +0100

    true tasker made. Waiting for action worker + need to specialized user experience

--- tasker/main_tasker.c
+++ tasker/main_tasker.c
@@ -9,9 +9,9 @@ int main()
     parsed_conf_file_successful(error);
     if (!error)
     {
-        // inform_user_about_loaded_params(params);
-        // ask_user_if_needed_details(params);
-        // ask_user_if_needed_bug_reported(params);
+        inform_user_about_loaded_params(params);
+        ask_user_if_needed_details(params);
+        ask_user_if_needed_bug_reported(params);
         int answer = ask_launch_tasks(params);
         if (answer)
         {
--- tasker/tasker.h
+++ tasker/tasker.h
@@ -5,5 +5,6 @@
 
 void tasker_launcher(true_parameters *params);
 void task_waiter(true_task *ttask);
+void action_waiter(true_action taction);
 
 #endif
