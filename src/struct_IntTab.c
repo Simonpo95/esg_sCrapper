a123a840333a8815ed7539ead6e270a26b508783
Simon <halimisimon@gmail.com>
2019-11-19 13:51:16 +0100

    ok

--- src/struct_IntTab.c
+++ src/struct_IntTab.c
@@ -1,26 +1,33 @@
 #include "struct_IntTab.h"
 
-IntTab *create_IntTab(IntWTagType * content_tab) {
-    struct IntTab *intTab = (struct IntTab *) malloc(sizeof(struct IntTab));
+IntTab *create_IntTab(IntWTagType *content_tab)
+{
+    struct IntTab *intTab = (struct IntTab *)malloc(sizeof(struct IntTab));
     intTab->size = 0;
     intTab->capacity = 1;
-    content_tab = (IntWTagType*) malloc(sizeof(IntWTagType) * intTab->capacity);
+    content_tab = (IntWTagType *)malloc(sizeof(IntWTagType) * intTab->capacity);
     intTab->content_tab = content_tab;
     return intTab;
 }
 
-void printIntTab(IntTab intTab) {
+void printIntTab(IntTab intTab)
+{
     printf("\n");
-    for (int i = 0; i < intTab.size; i++) {
+    for (int i = 0; i < intTab.size; i++)
+    {
         printf("%d = ", i);
         printIntWTagType(intTab.content_tab[i]);
-    printf("fin printf intTab\n");
-    printf("\n");
+        printf("fin printf intTab\n");
+        printf("\n");
+    }
 }
-void addToIntTab(IntTab *intTab, IntWTagType *intWTagType) {
+
+void addToIntTab(IntTab *intTab, IntWTagType *intWTagType)
+{
     intTab->content_tab[intTab->size] = *intWTagType;
     intTab->size++;
-    if (intTab->size >= intTab->capacity) {
+    if (intTab->size >= intTab->capacity)
+    {
         intTab->capacity *= 2;
         intTab->content_tab = realloc(intTab->content_tab, sizeof(IntWTagType) * intTab->capacity);
     }
