6c9f43028ebaec9ab84f9b0b6acb3ee16e8a3a30
Simon <halimisimon@gmail.com>
2019-11-19 10:24:59 +0100

    typecheck null error fixed

--- src/curlStrTab.c
+++ src/curlStrTab.c
@@ -1,6 +1,6 @@
 #include "curlStrTab.h"
 
-char* url_to_filable(char * url)
+char *url_to_filable(char *url)
 {
 
     char *dada_str = "dada";
@@ -12,7 +12,12 @@ char* url_to_filable(char * url)
     char *extension = calloc(sizeof(char) * 50, sizeof(char));
 
     result = typecheck(url);
-    printf("type check = %s\n", result);
+    fprintf(stderr,"type check = a%sa\n", result);
+    if (result == NULL)
+    {
+        result = "dada";
+    }
+    fprintf(stderr, "%s\n", result);
     result = remove_all_after_character(result, 59);
     printf("type check after remove = %s\n", result);
 
@@ -44,7 +49,6 @@ char* url_to_filable(char * url)
     return file_name;
 }
 
-
 //StrTab *curlStrTab(StrTab *url_strTab, char *principal_directory_path)
 StrTab *curlStrTab(StrTab *url_strTab)
 {
@@ -53,21 +57,21 @@ StrTab *curlStrTab(StrTab *url_strTab)
 
     for (int i = 0; i < url_strTab->size; i++)
     {
-        char * file_name_from_url = url_to_filable(url_strTab->content_tab[i]); // retourne le nom du fichier correspondant a un url
-        char * principal_directory_path = calloc(sizeof(char) * strlen(DIR_ROOT), sizeof(char));
+        char *file_name_from_url = url_to_filable(url_strTab->content_tab[i]); // retourne le nom du fichier correspondant a un url
+        char *principal_directory_path = calloc(sizeof(char) * strlen(DIR_ROOT), sizeof(char));
 
         strcpy(principal_directory_path, DIR_ROOT);
 
-        char *principal_directory_path_with_file_name = calloc(sizeof(char) * (strlen(file_name_from_url) + strlen(principal_directory_path)), sizeof(char));      // creation d'un char pour nom fichier ou dossier
+        char *principal_directory_path_with_file_name = calloc(sizeof(char) * (strlen(file_name_from_url) + strlen(principal_directory_path)), sizeof(char)); // creation d'un char pour nom fichier ou dossier
 
         sprintf(principal_directory_path_with_file_name, "%s%s", principal_directory_path, file_name_from_url);
 
         if (!(addToStrTab(filename_strTab, file_name_from_url)))
         {
-            fprintf(stderr, "path = %s\n",principal_directory_path_with_file_name);
-            fprintf(stderr, "url = %s\n\n",url_strTab->content_tab[i]);
+            fprintf(stderr, "path = %s\n", principal_directory_path_with_file_name);
+            fprintf(stderr, "url = %s\n\n", url_strTab->content_tab[i]);
             curlit(principal_directory_path_with_file_name, url_strTab->content_tab[i]);
-//            fprintf(stderr,"\n");
+            //            fprintf(stderr,"\n");
         }
     }
 
--- src/curler.c
+++ src/curler.c
@@ -1,18 +1,20 @@
 #include "curler.h"
 
-size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
+size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream)
+{
     size_t written = fwrite(ptr, size, nmemb, stream);
     return written;
 }
 
-
-int curlit(char * filepath, char * url){
-    CURL * curl;
-    FILE * f;
+int curlit(char *filepath, char *url)
+{
+    CURL *curl;
+    FILE *f;
     CURLcode res;
     int size = 0;
     curl = curl_easy_init();
-    if(curl) {
+    if (curl)
+    {
         f = fopen(filepath, "w+");
         curl_easy_setopt(curl, CURLOPT_URL, url);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
@@ -23,23 +25,26 @@ int curlit(char * filepath, char * url){
     curl_easy_cleanup(curl);
 }
 
-char * typecheck(char * url){
-    CURL * curl;
-    char * content_type;
+char *typecheck(char *url)
+{
+    CURL *curl;
+    char *content_type;
     CURLcode response;
-    FILE * f;
+    FILE *f;
     curl = curl_easy_init();
-    if(curl){
+    if (curl)
+    {
         f = fopen("trash.txt", "w+");
         curl_easy_setopt(curl, CURLOPT_URL, url);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, f);
         response = curl_easy_perform(curl);
-        if(!response){
-            remove("trash.txt");
+        if (!response)
+        {
             response = curl_easy_getinfo(curl, CURLINFO_CONTENT_TYPE, &content_type);
             return content_type;
         }
+        remove("trash.txt");
     }
     return "dada";
 }
